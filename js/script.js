2a584700287032bca036e0ed84e571b3ab7fde3a
Syllayoussouf <youssousylla552@gmail.com>
2026-02-22 22:54:13 +0000

    ✅Ajout de l'activation des liens et du scroll automatique vers ces dernier

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Activation du lien
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionheight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
